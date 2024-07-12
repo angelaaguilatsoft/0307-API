b7928dde2dd46ed74ac90abda1fef14175b86752
Angela Aguila Nunez <angela.aguila.tsoft@gmail.com>
2024-07-12 11:24:58 -0400

    reversa fechas en gráfico

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,74 @@
-const apiURL = 'https://mindicador.cl/api/'
+const apiURL = "https://mindicador.cl/api/";
+const tipomoneda = document.getElementById("tipoMoneda").value;
 
 async function getMonedas() {
-    try {
-        console.log(`Ingreso para obtener los datos de la API`);
-        const res = await fetch(apiURL);
-        const monedas = await res.json();
-        return(monedas);
-    } catch (e) {
-        console.log(`ERROR para obtener los datos de la API`);
-        const errorSpan = document.getElementById('error');
-        errorSpan.innerHTML = `Algo salió mal! Error: ${e.message}`;
-    }
+    // Obtener datos de la API
+    const res = await fetch(apiURL);
+    const monedas = await res.json();
+    return monedas;
 }
 
 async function renderMonedas() {
-    const resultado = document.getElementById('resultado'); // Mostrará el total convertido
-    const pesos = document.getElementById('pesos').value; // Pesos CL a convertir
-    const tipomoneda = document.getElementById('tipoMoneda').value; // Dolar, euro o UF
+  try {
+    const pesos = document.getElementById("pesos").value; // Pesos CL a convertir
+    // const tipomoneda = document.getElementById("tipoMoneda").value; // Dolar, euro o UF
+    const resultado = document.getElementById("resultado"); // Resultado del total convertido
     const monedas = await getMonedas();
     valoraConvertir = monedas[tipomoneda].valor;
-    resultadoFinal = pesos/valoraConvertir;
+    resultadoFinal = pesos / valoraConvertir;
     resultado.innerHTML = `<p>Total: ${resultadoFinal.toFixed(2)}</p>`;
     renderGrafica(tipomoneda);
+  } catch (error) {
+    console.log(`ERROR al obtener los datos de la API`);
+    const errorSpan = document.getElementById("error");
+    errorSpan.innerHTML = `<p><strong>*** Error: ${error.message} ***</strong></p>`;
+  }
 }
-
 
 async function getTipoMonedas(tipomoneda) {
-    const apiURL2 = apiURL + tipomoneda;
-    const res = await fetch(apiURL2);
-    const monedas = await res.json();
-     return(monedas);
+  const apiURL2 = apiURL + tipomoneda;
+  const res = await fetch(apiURL2);
+  const monedas = await res.json();
+  const monedas10 = (monedas.serie.slice(0, 10)).reverse();
+  return monedas10;
 }
 
-function prepararConfiguracionParaLaGrafica(monedas) {
-    // Creamos las variables necesarias para el objeto de configuración
-    const tipoDeGrafica = "line";
-    const titulo = "Monedas";
-    const colorDeLinea = "blue";
-    let monedas10 = monedas.serie.slice(0,10); // arreglo solo con los ultimos 10 registros
-    const nombresDeLasMonedas = monedas10.map((moneda) => moneda.fecha.substring(0,10)); // Datos en Eje X
-    const valores = monedas10.map((moneda) => moneda.valor); // Datos en Eje Y
-    // Creamos el objeto de configuración usando las variables anteriores
-    const config = {
-        type: tipoDeGrafica,
-        data: {
-            labels: nombresDeLasMonedas, // Eje X
-            datasets: [
-                {
-                    label: titulo,
-                    backgroundColor: colorDeLinea, // color gráfico 
-                    data: valores // Eje Y
-                }
-            ]
-        }
-    };
-    return config;
+function prepararConfiguracionParaLaGrafica(monedas10) {
+  // Creamos las variables necesarias para el objeto de configuración
+  const tipoDeGrafica = "line";
+  const titulo = "Moneda: ";
+  const colorDeLinea = "blue";
+//   let monedas10 = monedas.serie.slice(0, 10); // arreglo solo con los ultimos 10 registros
+  const nombresDeLasMonedas = monedas10.map((moneda) => moneda.fecha.substring(0, 10)); // Datos en Eje X
+  const valores = monedas10.map((moneda) => moneda.valor); // Datos en Eje Y
+  // Crear objeto de configuración usando variables anteriores
+  const config = {
+    type: tipoDeGrafica,
+    data: {
+      labels: nombresDeLasMonedas, // Eje X
+      datasets: [
+        {
+          label: `${titulo} ${tipomoneda.toUpperCase()}`,
+          backgroundColor: colorDeLinea, // color gráfico
+          data: valores, // Eje Y
+        },
+      ],
+    },
+  };
+  return config;
 }
 
 async function renderGrafica(tipomoneda) {
-    const monedas = await getTipoMonedas(tipomoneda);
-    const config = prepararConfiguracionParaLaGrafica(monedas);
-    const chartDOM = document.getElementById("myChart");
-    new Chart(chartDOM, config);
-}                
+    try {
+        const monedas = await getTipoMonedas(tipomoneda);
+        const config = prepararConfiguracionParaLaGrafica(monedas);
+        const chartDOM = document.getElementById("myChart");
+        console.log(chartDOM);
+        new Chart(chartDOM, config);        
+    } catch (error) {
+        console.log(`ERROR al obtener los datos de la API`);
+        const errorSpan = document.getElementById("errorGraf");
+        errorSpan.innerHTML = `<p><strong>*** Error al generar gráfico: ${error.message} ***</strong></p>
+        `;
+    }
+}
